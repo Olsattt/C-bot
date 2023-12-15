1f827c14da2e80a5c4cbd564571a346c8f3f2b93
Олег Куваев <112551191+Olsattt@users.noreply.github.com>
2023-12-15 13:23:20 +0300

    Add files via upload

---
+++ MainClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace RebootBot
+
+{
+    internal class MainClass
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            
+            var client = new TelegramBotClient("6774056759:AAFtPJAxq-ZyE34J0W_JjdNLPIMu7bV2od4");
+            client.StartReceiving(UpdateObject.Update, Error);
+            
+            
+            Console.ReadLine();
+        }
+
+
+
+        async static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            
+        }
+    }
+
+}
---
+++ UpdateObject.cs
@@ -0,0 +1,100 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace RebootBot
+{
+    internal class UpdateObject
+    {
+        private static string typeTask;
+        public async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+           
+           var message = update.Message;
+            if (message.Text != null)
+            {
+                var replyKeyboardMarkup = new ReplyKeyboardMarkup(new[]
+                                   {
+                        new KeyboardButton[] { "Создать задание","Просмотреть задания"},
+                        new KeyboardButton[] { "Что может бот","Как бот работает"}
+                    })
+                {
+                    ResizeKeyboard = true
+                };
+                var sentMessage = await botClient.SendTextMessageAsync(
+                       chatId: message.Chat.Id,
+                       text: "Выберите команду:",
+                       replyMarkup: replyKeyboardMarkup);
+                if (message.Text == "Создать задание")
+                {
+                     sentMessage = await botClient.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: "Опишите задание",
+                        replyMarkup: replyKeyboardMarkup);
+                    return;
+                }
+
+                if (message.Text == "Просмотреть задания")
+                {
+                    await using Stream stream = System.IO.File.OpenRead("C:\\Users\\Oleg\\Desktop\\Заказ.txt");
+                    message = await botClient.SendDocumentAsync(
+                       chatId: message.Chat.Id,
+                       document: InputFile.FromStream(stream: stream, fileName: "Заказ.txt"));
+                }
+
+                if (message.Text == "Что может бот")
+                {
+                    WhatBotJust(message, botClient);
+                }
+
+                if (message.Text == "Как бот работает")
+                {
+                    HowAreBotWork(message,botClient);
+                }
+
+                if (sentMessage != null)
+                {
+                    // Сохранение текста задачи
+                    await SaveMessageToFile(message, botClient);
+                    await botClient.SendTextMessageAsync(message.Chat.Id,"Задание создано");
+                }
+            }
+        }
+
+
+        public async static Task CreateTask(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+           
+
+        }
+
+        public async static Task SaveMessageToFile(Message message, ITelegramBotClient botClient)
+        {
+            string textFilePath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\Заказ.txt";
+
+            using StreamWriter streamWriter = new StreamWriter(textFilePath, true);
+            await streamWriter.WriteLineAsync("Username: " + "@" + message.Chat.Username);
+            await streamWriter.WriteLineAsync("Имя: " + message.Chat.FirstName);
+            await streamWriter.WriteLineAsync("Описание заказа: " + message.Text);
+            streamWriter.Close();
+            return;
+
+        }
+
+            public async static Task HowAreBotWork(Message message, ITelegramBotClient botClient)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, "Бот работает следующим образом: пользователь оставляет задание которое ему нужно выполнить,затем форммируется документ и отправляется другим пользователям бота");
+            return;
+        }
+        public async static Task WhatBotJust(Message message, ITelegramBotClient botClient)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, "Данный бот может сохранять получаемые сообщения в виде файла формируя заказ и отправляя его всем пользователям бота");
+            return;
+        }
+
+    }
+
+}
+
+    
+
